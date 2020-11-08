758bce4e03385d195e48122c26a66263e5e3a2ca
root <ouleur000@gmail.com>
2020-11-08 01:26:29 +0000

    autoupdate

--- app/api/v1_0/users.py
+++ app/api/v1_0/users.py
@@ -34,7 +34,7 @@ def get_users():
     return jsonify({ 'users': [user.to_json() for user in users] })
 
 
-@api.route('/git_update/',methods=['GET'])
+@api.route('/git_update',methods=['GET'])
 def git_update():
     home_dir = subprocess.run(["git","pull"],stdout=subprocess.PIPE, text=True)
     current_app.logger.info(home_dir.stdout)
