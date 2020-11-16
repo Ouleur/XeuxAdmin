9277ee1cd7d0267e2185f5c8b9ad3db798e0c7ab
root <ouleur000@gmail.com>
2020-11-16 07:22:44 +0000

    url

--- app/static/js/app/guichet.js
+++ app/static/js/app/guichet.js
@@ -1,3 +1,5 @@
+web_url = $("#container_cont").data('url');
+
 // TICKET_VIEW
 $("body").delegate('.ticket_view','click',function(){
     $("#numero-t").html($(this).data("id").split(":")[1]);
@@ -12,7 +14,7 @@ $("body").delegate('.item-action','click',function(){
 
 // SUIVANT
 $(".suivant").on('click',function(){
-    fetch("/next_ticket"+"/"+$("#nb-ticket").attr("data-id")).then(function(response){
+    fetch(web_url+"/next_ticket"+"/"+$("#nb-ticket").attr("data-id")).then(function(response){
         response.json().then(function(data){
             $("#numero-t").html(data.ticket[0].servstr+""+data.ticket[0].numero);
             $("#numero-t").attr("data-id",data.ticket[0].id);
@@ -23,19 +25,19 @@ $(".suivant").on('click',function(){
 
 // APPEL
 $(".appel").on('click',function(){
-    fetch("/call_ticket/"+$("#numero-t").attr("data-id")+"/"+$("#nb-ticket").attr("data-id")).then(function(response){});
+    fetch(web_url+"/call_ticket/"+$("#numero-t").attr("data-id")+"/"+$("#nb-ticket").attr("data-id")).then(function(response){});
     // $("#numero-t").html($(this).data("id").split(":")[1]);
 })
 
 // RAPPEL
 $(".rappel").on('click',function(){
-    fetch("/recall_ticket/"+$("#numero-t").attr("data-id")+"/"+$("#nb-ticket").attr("data-id")).then(function(response){});
+    fetch(web_url+"/recall_ticket/"+$("#numero-t").attr("data-id")+"/"+$("#nb-ticket").attr("data-id")).then(function(response){});
     $("#numero-t").html($(this).data("id").split(":")[1]);
 })
 
 // TRANSFERT
 $(".transfert").on('click',function(){
-    fetch("/transfert_ticket/"+$("#numero-t").attr("data-id")).then(function(response){});
+    fetch(web_url+"/transfert_ticket/"+$("#numero-t").attr("data-id")).then(function(response){});
     $("#numero-t").html($(this).data("id").split(":")[1]);
 })
 
@@ -51,7 +53,6 @@ $(".fermer").on('click',function(){
 
 function tickets_list(){
     url_code = window.location.pathname.split('/');
-    web_url = $("#container_cont").data('url');
     const eventSource = new EventSource(web_url+"/.well-known/mercure?topic=https://example.com/tickets/"+url_code[3]+"/"+url_code[4]);
     eventSource.onmessage = ({ data }) => {
         console.log(JSON.parse(data));
