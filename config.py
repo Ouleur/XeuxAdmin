c714aacc2bb764dd24fa18e16356e48e953f4123
root <ouleur000@gmail.com>
2020-11-24 16:32:45 +0000

    Config

--- config.py
+++ config.py
@@ -5,12 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess strin'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    PO_MAIL_SUBJECT_PREFIX = '[Podipo]'
-    PO_MAIL_SENDER = 'Podipo Admin <info@podipo.com>'
-    PO_ADMIN = os.environ.get('PO_ADMIN') or 'admin@popodipo.com'
-
+    PO_MAIL_SUBJECT_PREFIX = '[VIT]'
+    PO_MAIL_SENDER = 'VIT Admin <info@filevit.com>'
+    PO_ADMIN = os.environ.get('PO_ADMIN') or 'admin@fileapp.com'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -45,13 +43,17 @@ class DevelopmentConfig(Config):
     POSTGRES_USER="odoo"
     POSTGRES_PW="odoo"
     WEB_URL="http://127.0.0.1:5000"
+    SITE_URL="http://127.0.0.1/vitapp"
+    SCREEN_URL="http://localhost:8080/"
     MERCURE_URL="http://127.0.0.1:3000"
     UPLOADS_DIR = "app/static/uploads/"
-
+    
 
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
 
 class TestingConfig(Config):
     TESTING = True
@@ -60,12 +62,45 @@ class TestingConfig(Config):
     POSTGRES_USER="odoo"
     POSTGRES_PW="odoo"
     WEB_URL="http://144.91.127.68/pozy"
+    SITE_URL="http://www.filevit.com"
+    SCREEN_URL="http://144.91.127.68/screen"
     MERCURE_URL="http://144.91.127.68:4040"
     UPLOADS_DIR = "/var/www/pozy/app/static/uploads/"
 
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     # SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class BetaConfig(Config):
+    DEBUG = True
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess strin'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    PO_MAIL_SUBJECT_PREFIX = '[VIT]'
+    PO_MAIL_SENDER = 'VIT Admin <infos@filevit.com >'
+    PO_ADMIN = os.environ.get('PO_ADMIN') or 'admin@fileapp.com'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'mail47.lwspanel.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "infos@filevit.com "
+    MAIL_PASSWORD = "01709902@Vit"
+
+    POSTGRES_DB = "popodipo_dev"
+    POSTGRES_URL="127.0.0.1:5432"
+    POSTGRES_USER="odoo"
+    POSTGRES_PW="odoo"
+    WEB_URL="htt://app.filevit.com"
+    SITE_URL="htt://www.filevit.com"
+    SCREEN_URL="htt://screen.filevit.com"
+    MERCURE_URL="http://app.filevit.com:4040"
+    UPLOADS_DIR = "/var/www/vitapp/app/static/uploads/"
+
+
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
 
 class ProdcutionConfig(Config):
     POSTGRES_DB = "popodipo_prod"
@@ -83,6 +118,7 @@ class ProdcutionConfig(Config):
 config = {
     'development' : DevelopmentConfig,
     'testing':TestingConfig,
+    'beta' : BetaConfig,
     'production':ProdcutionConfig,
     'default':DevelopmentConfig
 }
