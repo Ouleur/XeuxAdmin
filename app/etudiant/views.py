849851ab6c5ca232079c8e627f91124769f47e9f
Ouleur <ouleur000@gmail.com>
2022-07-01 05:14:42 +0000

    verif student

--- app/etudiant/views.py
+++ app/etudiant/views.py
@@ -20,7 +20,7 @@ def etudiant_infos():
    
    if request.method == 'POST':
    
-      etudiant = Etudiant.query.filter_by(matricule=request.form['matricule']).first()
+      etudiant = EtudiantVerif.query.filter_by(matricule=request.form['matricule']).first()
       if etudiant:
          print()
          if session.get('etudiant'):
@@ -44,7 +44,7 @@ def etudiant_resultat():
    etudiant = session['etudiant']
    
    if request.method == 'POST':
-      et = Etudiant.query.filter_by(id=etudiant['id']).first()
+      et = EtudiantVerif.query.filter_by(id=etudiant['id']).first()
       print(et.id)
       if request.files['image']:
             data = request.files['image']
@@ -71,10 +71,45 @@ def etudiant_resultat():
    return render_template('etudiant_result.html', etudiant=etudiant, filieres=form_filieres, niveau=niveau, groupes=groupes,antennes=antennes)
 
 
+@etudiant.route('/etudiant/verif/add', methods=['POST','GET'])
+def etudiant_add():
+   form =request.get_json() or {}
+   print(form)
+   form = form["root"]
+   filiere = Filiere.query.filter_by(denomination=form['filiere']).first()
+
+   if filiere:
+      form['filiere'] =  filiere.id
+   else:
+      filiere = Filiere(denomination=form['filiere'])
+      db.session.add(filiere)
+      db.session.commit()
+      form['filiere'] =  filiere.id
+
+
+
+   # filename = None
+   # if request.files['image']:
+   #    data = request.files['image']
+   #    filename = secure_filename(data.filename)
+   #    if not exists(os.path.join(uploads_dir, filename)):
+   #       data.save(os.path.join(uploads_dir, filename))
+      
+   qrcode = generate_qr(form['matricule'])
+   try:
+      etudiant = EtudiantVerif(matricule=form['matricule'],nom=form['nom'],prenoms=form['prenoms'],filiere_id=form['filiere'],niveau=form['niveau'],date_naissance=form['date_naissance'],statut=form['statut'],antenne=form['antenne'],photo=form['photo'])
+      db.session.add(etudiant)
+      db.session.commit()
+   except:
+      db.session.rollback()
+      print("error")
+
+   return jsonify(etudiant.to_json())
+
 @etudiant.route('/generate/qrcode', methods=['POST'])
 def qrcode_generate():
    et = session['etudiant']
-   etudiant = Etudiant.query.filter_by(id=et.get('id')).first()
+   etudiant = EtudiantVerif.query.filter_by(id=et.get('id')).first()
 
    qrcode = generate_qr(etudiant.matricule)
    etudiant.qrcode = qrcode
@@ -100,7 +135,7 @@ def etudiant_new():
             data.save(os.path.join(uploads_dir, filename))
       
       qrcode = generate_qr(form.matricule.data)
-      etudiant = Etudiant(matricule=form.matricule.data,nom=form.nom.data,prenoms=form.prenoms.data,filiere_id=form.filiere.data,niveau=form.niveau.data,date_naissance=datetime.strftime(form.date_naissance.data, '%d/%m/%Y'),card_id=form.id_carte.data,antenne=form.antenne.data,groupe=form.groupe.data, photo=filename, qrcode=qrcode)
+      etudiant = EtudiantVerif(matricule=form.matricule.data,nom=form.nom.data,prenoms=form.prenoms.data,filiere_id=form.filiere.data,niveau=form.niveau.data,date_naissance=datetime.strftime(form.date_naissance.data, '%d/%m/%Y'),card_id=form.id_carte.data,antenne=form.antenne.data,groupe=form.groupe.data, photo=filename, qrcode=qrcode)
       db.session.add(etudiant)
       db.session.commit()
 
