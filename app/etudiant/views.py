34c1852d7c2c4bd3a45b6068a43c76b863087312
Ouleur <ouleur000@gmail.com>
2022-10-31 01:08:31 +0000

    check result

--- app/etudiant/views.py
+++ app/etudiant/views.py
@@ -15,7 +15,7 @@ from sqlalchemy.exc import IntegrityError
 
 
 
-@etudiant.route('/etudiant/infos2022', methods=['POST','GET'])
+@etudiant.route('/etudiant/infos', methods=['POST','GET'])
 def etudiant_infos():
    form = EtudiantForm(request.form)
    
@@ -33,6 +33,25 @@ def etudiant_infos():
       
       # return redirect(url_for('etudiant.etudiant_resultat'))
    return render_template('etudiant_infos.html',form=form)
+
+@etudiant.route('/etudiant/etudiant_check_infos', methods=['POST','GET'])
+def etudiant_check_infos():
+   form = EtudiantForm(request.form)
+   
+   if request.method == 'POST':
+   
+      etudiant = EtudiantVerif.query.filter_by(matricule=request.form['matricule']).first()
+      if etudiant:
+         print()
+         if session.get('etudiant'):
+            session.pop('etudiant')
+         session['etudiant'] = etudiant.to_json()
+         return jsonify({'message' : 'succes'}), 200
+      else:
+         return jsonify({'message' : 'matricule invalide'}), 404 
+      
+      # return redirect(url_for('etudiant.etudiant_resultat'))
+   return render_template('etudiant_check_infos.html',form=form)
 
 @etudiant.route('/etudiant/result', methods=['POST','GET'])
 def etudiant_resultat():
@@ -67,8 +86,39 @@ def etudiant_resultat():
    
    return render_template('etudiant_result.html', etudiant=etudiant, filieres=form_filieres, niveau=niveau, groupes=groupes,antennes=antennes)
 
+@etudiant.route('/etudiant/check_result', methods=['POST','GET'])
+def etudiant_check_resultat():
+   uploads_dir = current_app.config['UPLOADS_DIR']
+   filieres = Filiere.query.all()
+   form_filieres = [(item.id, item.denomination) for item in filieres]
+   antennes = ['ABIDJAN','BOUAKE','ABOISSO','KORHOGO',"ABENGOUROU","DALOA"]
+   groupes = ['Groupe A','Groupe B','Groupe C','Groupe D',"Groupe E","Groupe F","Groupe G","Groupe H","Groupe I","Groupe J","Groupe K"]
+   niveau = ['Licence 1','Licence 2','Licence 3']
+   etudiant = session['etudiant']
+   
+   if request.method == 'POST':
+      et = EtudiantVerif.query.filter_by(id=etudiant['id']).first()
+      
+      if request.files['image']:
+            print(request.files['image'].content_type)
+            data = request.files['image']
+            filename = secure_filename("{}.{}".format(et.matricule,(request.files['image'].filename.split('.')[-1])))
+            if not exists(os.path.join(uploads_dir, filename)):
+               data.save(os.path.join(uploads_dir, filename))
+            et.photo = filename
+            
+      et.date_naissance = request.form['date_naissance_edit']
+      et.lieu_naissance = request.form['lieu_naissance']
+      db.session.add(et)
+      db.session.commit()
+      session.pop('etudiant')
+      session['etudiant'] = et.to_json()
+      print(etudiant)
+      return redirect(url_for('etudiant.etudiant_check_resultat'))
+   
+   return render_template('etudiant_result_check.html', etudiant=etudiant, filieres=form_filieres, niveau=niveau, groupes=groupes,antennes=antennes)
 
-@etudiant.route('/etudiant/verif/add', methods=['POST','GET'])
+@etudiant.route('/etudiant/verif/add2022', methods=['POST','GET'])
 def etudiant_add():
    form =request.get_json() or {}
    print(form)
@@ -116,8 +166,7 @@ def qrcode_generate():
    session['etudiant'] = etudiant.to_json()
    return redirect(url_for('etudiant.etudiant_resultat'))
 
-
-@etudiant.route('/etudiant/new2022', methods=['POST','GET'])
+@etudiant.route('/etudiant/new', methods=['POST','GET'])
 def etudiant_new():
    uploads_dir = current_app.config['UPLOADS_DIR']
    filieres = Filiere.query.all()
@@ -127,16 +176,22 @@ def etudiant_new():
       filename = None
       if request.files['image']:
          data = request.files['image']
-         filename = secure_filename(data.filename)
+         print(data)
+         filename = "{}.png".format(form.matricule.data)
          if not exists(os.path.join(uploads_dir, filename)):
             data.save(os.path.join(uploads_dir, filename))
       
-      qrcode = generate_qr(form.matricule.data)
-      etudiant = EtudiantVerif(matricule=form.matricule.data,nom=form.nom.data,prenoms=form.prenoms.data,filiere_id=form.filiere.data,niveau=form.niveau.data,date_naissance=datetime.strftime(form.date_naissance.data, '%d/%m/%Y'),card_id=form.id_carte.data,antenne=form.antenne.data,groupe=form.groupe.data, photo=filename, qrcode=qrcode)
-      db.session.add(etudiant)
-      db.session.commit()
+         qrcode = generate_qr(form.matricule.data)
+         etudiant = EtudiantVerif(matricule=form.matricule.data,nom=form.nom.data,prenoms=form.prenoms.data,filiere_id=form.filiere.data,niveau=form.niveau.data,date_naissance=datetime.strftime(form.date_naissance.data, '%d/%m/%Y'),card_id=form.id_carte.data,antenne=form.antenne.data,groupe=form.groupe.data, photo=filename, qrcode=qrcode)
+         db.session.add(etudiant)
+         db.session.commit()
 
-      return redirect(url_for('etudiant.etudiant_infos'))
+         flash('Hello {} {}, votre enregistrement a été éffectué !<br><a href="/etudiant/etudiant_check_infos">Cliquez ici pour verifier</a>'.format(form.nom.data,form.prenoms.data))
+         return redirect(url_for('etudiant.etudiant_new'))
+
+      else:
+         flash('Hello {} {}, veuillez selectionner votre photo !'.format(form.nom.data,form.prenoms.data))
+         
 
       
    return render_template('etudiant_new.html',form=form)
