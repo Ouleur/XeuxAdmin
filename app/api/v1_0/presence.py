6580e434b1bae318f68bdc6264f298f8c6209a7f
MacBook Pro <macbookpro@192.168.1.2>
2024-04-30 02:11:06 +0000

    Add https://www.app-infas.net/scolarite/api/presence

--- app/api/v1_0/presence.py
+++ app/api/v1_0/presence.py
@@ -11,6 +11,7 @@ import csv
 import io
 import os
 from ...utilities import *
+import requests
 
 
 auth = HTTPBasicAuth()
@@ -68,6 +69,13 @@ def add_mtle_presence():
                  
     if etudiant:
         presence = Presence(etudiant_id=etudiant.id,filiere_id=etudiant.filiere_id,niveau=etudiant.niveau,date_badge=data["Date"],date=data["Date"].split(" ")[0])
+    
+        url = 'https://www.app-infas.net/scolarite/api/presence'
+        myobj = {'matricule': presence.matricule}
+
+        x = requests.post(url, json = myobj)
+
+        print(x.text)
         db.session.add(presence)
         db.session.commit()
 
